23b5d207d30f3c540c7bbf5330e0bbb058e4f199
Antonio Jefferson <antoniojeferson96@gmail.com>
2021-11-18 02:55:56 -0300

    refactor(municipe, adress): Adicionando o has_many do municipe e craindo a def json para melhor apresentar os dados no controller

--- app/models/adress.rb
+++ app/models/adress.rb
@@ -1,3 +1,18 @@
 class Adress < ApplicationRecord
   belongs_to :municipe
+
+  def json
+    {
+			cep: self.cep,
+			logradouro: self.logradouro,
+			complement: self.complement,
+			district: self.district,
+			city: self.city,
+			uf: self.uf,
+			ibge_code: self.ibge_code,
+			created_at: self.created_at,
+			updated_at: self.updated_at,
+			municipe: self.municipe,
+		}
+  end
 end
--- app/models/municipe.rb
+++ app/models/municipe.rb
@@ -1,3 +1,4 @@
 class Municipe < ApplicationRecord
+    has_many :adress, foreign_key: "municipe_id"
     mount_uploader :photo, ImageUploader 
 end
