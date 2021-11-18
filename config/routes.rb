0c98051cdbdf1b28810d43fdf5d7fe2b01f38b8b
Antonio Jefferson <antoniojeferson96@gmail.com>
2021-11-18 02:36:57 -0300

    refactor(routes): Adicionando as rotas de municipes no arquivo routes em config/

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # Routes municipes
+  get '/municipes', to: 'municipes#index'
+  get '/municipes/:_id', to: 'municipes#show'
+  post '/municipes', to: 'municipes#create'
+  put '/municipes/:_id', to: 'municipes#update'
 end
