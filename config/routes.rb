92529a02d75e05656caf70059c3661c7a06747d0
Antonio Jefferson <antoniojeferson96@gmail.com>
2021-12-15 16:30:16 -0300

    refactor(routes.rb): corrigindo as rotas para o novo esquema
    Corrigindo as rotas para a nova nomenclatura no esquema de models do projeto.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-   # Routes adresses
-   get '/adresses', to: 'adresses#index'
-   get '/adresses/:_id', to: 'adresses#show'
-   post '/adresses', to: 'adresses#create'
-   put '/adresses/:_id', to: 'adresses#update'
+   # Routes addresses
+   get '/address', to: 'addresses#index'
+   get '/address/:_id', to: 'addresses#show'
+   post '/address', to: 'addresses#create'
+   put '/address/:_id', to: 'addresses#update'
 
-  # Routes municipes
-  get '/municipes', to: 'municipes#index'
-  get '/municipes/:_id', to: 'municipes#show'
-  post '/municipes', to: 'municipes#create'
-  put '/municipes/:_id', to: 'municipes#update'
+  # Routes citizens
+  get '/citizens', to: 'citizens#index'
+  get '/citizen/:_id', to: 'citizens#show'
+  post '/citizen', to: 'citizens#create'
+  put '/citizen/:_id', to: 'citizens#update'
 end
