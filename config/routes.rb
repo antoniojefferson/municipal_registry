96a2123ae182e3b60aa50a8e3ea7075e7a3287f5
Antonio Jefferson <antoniojeferson96@gmail.com>
2021-12-16 08:32:43 -0300

    refactor(routes.rb): simplificando as rotas, usando o resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # Routes citizens
-  get '/citizens', to: 'citizens#index'
-  get '/citizen/:_id', to: 'citizens#show'
-  post '/citizen', to: 'citizens#create'
-  put '/citizen/:_id', to: 'citizens#update'
+  resources :citizens, except: :destroy
 end
