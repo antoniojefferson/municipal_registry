c05e2fc8f435aab0b2e22a3706ab49716c585181
NBOM30DEV10 <antoniojeferson96@gmail.com>
2021-12-20 11:43:47 -0300

    refactor: corrigindo identacoes

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,23 +1,23 @@
 class Citizen < ApplicationRecord
-    has_many :addresses
-    mount_uploader :photo, ImageUploader
-    accepts_nested_attributes_for :addresses, allow_destroy: true
-    validates :full_name, :cpf, :cns, :email, :birth_date, :phone, :photo, :status, presence: true
+  has_many :addresses
+  mount_uploader :photo, ImageUploader
+  accepts_nested_attributes_for :addresses, allow_destroy: true
+  validates :full_name, :cpf, :cns, :email, :birth_date, :phone, :photo, :status, presence: true
 
-    def json
-      {
-        id: self.id,
-        full_name: self.full_name,
-        cpf: self.cpf,
-        cns: self.cns,
-        email: self.email,
-        birth_date: self.birth_date.strftime('%d/%m/%Y'),
-        phone: self.phone,
-        photo: self.photo,
-        status: self.status,
-        addresses: self.addresses,
-        created_at: self.created_at,
-        updated_at: self.updated_at,
-      }
-    end
+  def json
+    {
+      id: self.id,
+      full_name: self.full_name,
+      cpf: self.cpf,
+      cns: self.cns,
+      email: self.email,
+      birth_date: self.birth_date.strftime('%d/%m/%Y'),
+      phone: self.phone,
+      photo: self.photo,
+      status: self.status,
+      addresses: self.addresses,
+      created_at: self.created_at,
+      updated_at: self.updated_at,
+    }
+  end
 end
