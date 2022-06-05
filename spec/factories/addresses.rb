77113604728e7f147474f1748a56a18b3164aec8
Antonio Jefferson <antoniojeferson96@gmail.com>
2022-06-05 13:26:41 -0300

    test(spec/factories/addresses.rb): mudando o nome do arquivo para o plural

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :address do
+    cep { FFaker::AddressBR.zip_code }
+    logradouro { FFaker::AddressBR.street }
+    complement { "Teste" }
+    district { 'Centro' }
+    city { FFaker::AddressBR.city }
+    uf { FFaker::AddressBR.state_abbr }
+    ibge_code { 123 }
+    citizen
+  end
+end
